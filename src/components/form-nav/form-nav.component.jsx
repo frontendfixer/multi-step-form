be7e869ef09cf7bb116e4238e75895e25e8e33f8
Lakshmikanta <frontendfixer@gmail.com>
2023-07-15 18:57:45 +0530

    component: form navigation with react-router outlet outlet allow us to insert other page at this position

--- src/components/form-nav/form-nav.component.jsx
+++ src/components/form-nav/form-nav.component.jsx
@@ -1,53 +1,47 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import style from './styles.module.scss';
 
 function FormNav() {
+  const stepValue = useSelector(state => state.step.value);
+
   return (
-    <ul className={style.nav__links}>
-      <li>
-        <Link to="/" className={style.nav__link}>
-          <div className={style.steps__index} data-active="true">
-            <span>1</span>
-          </div>
-          <div className={style.steps__details}>
-            <span>step 1</span>
-            <p>Your Info</p>
-          </div>
-        </Link>
+    <ul className={style.nav__items}>
+      <li className={style.nav__item} data-active={stepValue === 1}>
+        <div className={style.steps__index}>
+          <span>1</span>
+        </div>
+        <div className={style.steps__details}>
+          <span>step 1</span>
+          <p>Your Info</p>
+        </div>
       </li>
-      <li>
-        <Link to="/user-plans" className={style.nav__link}>
-          <div className={style.steps__index}>
-            <span>2</span>
-          </div>
-          <div className={style.steps__details}>
-            <span>step 2</span>
-            <p>select plan</p>
-          </div>
-        </Link>
+      <li className={style.nav__item} data-active={stepValue === 2}>
+        <div className={style.steps__index}>
+          <span>2</span>
+        </div>
+        <div className={style.steps__details}>
+          <span>step 2</span>
+          <p>select plan</p>
+        </div>
       </li>
-      <li>
-        <Link to="/add-ons" className={style.nav__link}>
-          <div className={style.steps__index}>
-            <span>3</span>
-          </div>
-          <div className={style.steps__details}>
-            <span>step 3</span>
-            <p>add-ons</p>
-          </div>
-        </Link>
+      <li className={style.nav__item} data-active={stepValue === 3}>
+        <div className={style.steps__index}>
+          <span>3</span>
+        </div>
+        <div className={style.steps__details}>
+          <span>step 3</span>
+          <p>add-ons</p>
+        </div>
       </li>
-      <li>
-        <Link to="/order-summary" className={style.nav__link}>
-          <div className={style.steps__index}>
-            <span>4</span>
-          </div>
-          <div className={style.steps__details}>
-            <span>step 4</span>
-            <p>summary</p>
-          </div>
-        </Link>
+      <li className={style.nav__item} data-active={stepValue === 4}>
+        <div className={style.steps__index}>
+          <span>4</span>
+        </div>
+        <div className={style.steps__details}>
+          <span>step 4</span>
+          <p>summary</p>
+        </div>
       </li>
     </ul>
   );
