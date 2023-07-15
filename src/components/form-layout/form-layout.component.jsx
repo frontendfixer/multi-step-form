d546f674d5944156dd2f6905f1deb56f164982e8
Lakshmikanta <frontendfixer@gmail.com>
2023-07-15 18:54:36 +0530

    component: form base layout

--- src/components/form-layout/form-layout.component.jsx
+++ src/components/form-layout/form-layout.component.jsx
@@ -1,6 +1,5 @@
 import { Outlet } from 'react-router-dom';
 
-import { ReactComponent as NavBackgroundImage } from '../../assets/images/bg-sidebar-desktop.svg';
 import FormNav from '../form-nav/form-nav.component';
 import style from './styles.module.scss';
 
@@ -8,9 +7,6 @@ function FormLayout() {
   return (
     <div className={style.form__container}>
       <div className={style.form__nav}>
-        <div className={style.nav__image}>
-          <NavBackgroundImage className={style.form__nav__image} />
-        </div>
         <div className={style.nav__container}>
           <FormNav />
         </div>
