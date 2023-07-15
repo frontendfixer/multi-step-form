a704aaef49f26013bf3c111c8af4b7ffd8d42e11
Lakshmikanta <frontendfixer@gmail.com>
2023-07-15 20:11:15 +0530

    add: postcss autoprefixer

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
 import { defineConfig } from 'vite';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), ViteImageOptimizer(), svgr()],
   server: {
     port: 5555,
     strictPort: true,
     open: true,
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
   },
   build: {
     minify: 'terser',
