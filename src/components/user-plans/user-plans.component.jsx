dfccf87236f4e6e5539403cbbab676d1d68ddcea
Lakshmikanta <frontendfixer@gmail.com>
2023-07-15 18:55:23 +0530

    component: user-plan page

--- src/components/user-plans/user-plans.component.jsx
+++ src/components/user-plans/user-plans.component.jsx
@@ -1,68 +1,189 @@
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import { ReactComponent as AdvanceIcon } from '../../assets/images/icon-advanced.svg';
 import { ReactComponent as ArcadeIcon } from '../../assets/images/icon-arcade.svg';
 import { ReactComponent as ProIcon } from '../../assets/images/icon-pro.svg';
-import Button from '../button/button.component';
+import { setPlan, toYearly } from '../../store/formSlice';
+import { getStep, nextStep, previousStep } from '../../store/stepSlice';
+import Button, { BUTTON_TYPE } from '../button/button.component';
 import style from './styles.module.scss';
 
 function UserPlans() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const { isYearly, yearlyDiscount, plan } = useSelector(state => state.form);
+  const yearlyFactor = 12 * (1 - yearlyDiscount);
+
+  const [userPlan, setUserPlan] = useState({
+    id: plan.id,
+    name: plan.name,
+    price: plan.price,
+  });
+
+  const createUserPlan = elID => {
+    const planEl = document.getElementById(elID).closest('label');
+    const planName = planEl.querySelector('h3').textContent;
+    const planPrice = +planEl.querySelector('.price').textContent;
+
+    setUserPlan(previousValue => ({
+      ...previousValue,
+      id: elID,
+      name: planName,
+      price: planPrice,
+    }));
+  };
+
+  const handelClickPrevious = () => {
+    navigate('/');
+    dispatch(previousStep());
+  };
+  const handelClickNext = () => {
+    navigate('/add-ons');
+    dispatch(nextStep());
+  };
+
+  const handelToggleToYear = () => {
+    dispatch(toYearly(!isYearly));
+    let newPrice;
+    if (isYearly) newPrice = Math.round(userPlan.price / yearlyFactor);
+    if (!isYearly) newPrice = Math.round(userPlan.price * yearlyFactor);
+
+    setUserPlan(previousValue => ({ ...previousValue, price: newPrice }));
+  };
+
+  const handelRadio = e => {
+    createUserPlan(e.target.id);
+  };
+
+  useEffect(() => {
+    dispatch(getStep(2));
+    document.getElementById(plan.id).checked = true;
+  }, []);
+  useEffect(() => {
+    dispatch(setPlan(userPlan));
+  }, [userPlan]);
+
   return (
-    <div className="form__body__container">
-      <div>
+    <form className="form-wrapper">
+      <div className="form__body__container">
         <h2>Select your plan</h2>
         <p>You have the option of monthly or yearly billing.</p>
-        <div className={style.plans__details}>
-          <div className={style.plan__cards}>
-            <label htmlFor="arcade" className={style.plan__card}>
-              <input type="radio" name="game-plan" id="arcade" />
-              <span>
-                <ArcadeIcon />
-              </span>
-              <div className={style.plan__price}>
-                <h3>Arcade</h3>
-                <span>$9/mo</span>
-              </div>
-            </label>
-            <label htmlFor="advance" className={style.plan__card}>
-              <input type="radio" name="game-plan" id="advance" />
-              <span>
-                <AdvanceIcon />
-              </span>
-              <div className={style.plan__price}>
-                <h3>Advanced</h3>
-                <span>$12/mo</span>
-              </div>
-            </label>
-            <label htmlFor="pro" className={style.plan__card}>
-              <input type="radio" name="game-plan" id="pro" />
-              <span>
-                <ProIcon />
-              </span>
-              <div className={style.plan__price}>
-                <h3>Pro</h3>
-                <span>$15/mo</span>
-              </div>
-            </label>
-          </div>
-          <div className={style.plan__tenure}>
-            <span data-tenure="active">Monthly</span>
-            <label htmlFor="toggle-checkbox" className={style.toggle__button}>
-              <input
-                className={style.toggle__checkbox}
-                type="checkbox"
-                name="plan-tenure"
-                id="toggle-checkbox"
-              />
-              <span className={style.toggle__ball} />
-            </label>
-            <span>Yearly</span>
-          </div>
+        <div className={style.plan__cards} onChange={handelRadio}>
+          <label htmlFor="arcade" className={style.plan__card}>
+            <input type="radio" name="game-plan" id="arcade" />
+            <span>
+              <ArcadeIcon />
+            </span>
+            <div className={style.plan__price}>
+              <h3>Arcade</h3>
+              {isYearly ? (
+                <>
+                  <span className="discount">{yearlyDiscount * 100}% off</span>
+                  <p className={style.plan__price__details}>
+                    $
+                    <span className="price">
+                      {Math.round(9 * 12 * (1 - yearlyDiscount))}{' '}
+                    </span>
+                    /yr
+                    <span className={style.free__months}>2 months free</span>
+                  </p>
+                </>
+              ) : (
+                <p className={style.plan__price__details}>
+                  $<span className="price">9</span>/mo
+                </p>
+              )}
+            </div>
+          </label>
+          <label htmlFor="advance" className={style.plan__card}>
+            <input type="radio" name="game-plan" id="advance" />
+            <span>
+              <AdvanceIcon />
+            </span>
+            <div className={style.plan__price}>
+              <h3>Advanced</h3>
+              {isYearly ? (
+                <>
+                  <span className="discount">{yearlyDiscount * 100}% off</span>
+                  <p className={style.plan__price__details}>
+                    $
+                    <span className="price">
+                      {Math.round(12 * 12 * (1 - yearlyDiscount))}{' '}
+                    </span>
+                    /yr
+                    <span className={style.free__months}>2 months free</span>
+                  </p>
+                </>
+              ) : (
+                <p className={style.plan__price__details}>
+                  $<span className="price">12</span>/mo
+                </p>
+              )}
+            </div>
+          </label>
+          <label htmlFor="pro" className={style.plan__card}>
+            <input type="radio" name="game-plan" id="pro" />
+            <span>
+              <ProIcon />
+            </span>
+            <div className={style.plan__price}>
+              <h3>Pro</h3>
+              {isYearly ? (
+                <>
+                  <span className="discount">{yearlyDiscount * 100}% off</span>
+                  <p className={style.plan__price__details}>
+                    $
+                    <span className="price">
+                      {Math.round(15 * 12 * (1 - yearlyDiscount))}{' '}
+                    </span>
+                    /yr
+                    <span className={style.free__months}>2 months free</span>
+                  </p>
+                </>
+              ) : (
+                <p className={style.plan__price__details}>
+                  $<span className="price">15</span>/mo
+                </p>
+              )}
+            </div>
+          </label>
+        </div>
+        <div className={style.plan__tenure}>
+          <span data-tenure="active">Monthly</span>
+          <label htmlFor="toggle-checkbox" className={style.toggle__button}>
+            <input
+              className={style.toggle__checkbox}
+              type="checkbox"
+              name="plan-tenure"
+              id="toggle-checkbox"
+              checked={isYearly}
+              onChange={handelToggleToYear}
+            />
+            <span className={style.toggle__ball} />
+          </label>
+          <span>Yearly</span>
         </div>
       </div>
       <div className="button__container">
-        <Button buttonType="inverted">Go back</Button>
-        <Button>Next Step</Button>
+        <Button
+          type="button"
+          buttonType={BUTTON_TYPE.PREVIOUS}
+          onClick={handelClickPrevious}
+        >
+          Go back
+        </Button>
+        <Button
+          type="submit"
+          buttonType={BUTTON_TYPE.NEXT}
+          onClick={handelClickNext}
+        >
+          Next Step
+        </Button>
       </div>
-    </div>
+    </form>
   );
 }
 
