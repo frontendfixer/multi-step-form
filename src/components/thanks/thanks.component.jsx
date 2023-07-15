f4ef42115186b84c915a6b87cc0eaa478f25b350
Lakshmikanta <frontendfixer@gmail.com>
2023-07-15 18:57:59 +0530

    component: form submit page

--- src/components/thanks/thanks.component.jsx
+++ src/components/thanks/thanks.component.jsx
@@ -1,24 +1,40 @@
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import { ReactComponent as ThanksIcon } from '../../assets/images/icon-thank-you.svg';
+import { resetForm } from '../../store/formSlice';
 import Button from '../button/button.component';
-import Spinner from '../spinner/spinner.component';
 import style from './styles.module.scss';
 
 function ThanksCard() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handelClose = () => {
+    dispatch(resetForm());
+    navigate('/');
+  };
+
   return (
-    <div className={style.container}>
-      <ThanksIcon className={style.thankYou__icon} />
-      <div className={style.thankYou__message}>
-        <h2>Thank you!</h2>
-        <p>
-          Thanks for confirming your subscription! We hope you have fun using
-          our platform. If you ever need support, please feel free to email us
-          at support@loremgaming.com.
-        </p>
+    <form className="form-wrapper">
+      <div className={`form__body__container ${style.container}`}>
+        <ThanksIcon className={style.thankYou__icon} />
+        <div className={style.thankYou__message}>
+          <h2>Thank you!</h2>
+          <p>
+            Thanks for confirming your subscription! We hope you have fun using
+            our platform. If you ever need support, please feel free to email us
+            at support@loremgaming.com.
+          </p>
+        </div>
       </div>
-      <Button buttonType="confirm">
-        <Spinner />
-      </Button>
-    </div>
+      <div className="button__container">
+        <Button buttonType="confirm" onClick={handelClose}>
+          {' '}
+          Close
+        </Button>
+      </div>
+    </form>
   );
 }
 
