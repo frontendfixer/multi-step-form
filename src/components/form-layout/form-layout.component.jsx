41dd7f7cec8605068cb5ab4764798a3e754aeda0
Lakshmikanta <frontendfixer@gmail.com>
2023-07-17 09:49:26 +0530

    feat: add h1 heading with visually-hidden class

--- src/components/form-layout/form-layout.component.jsx
+++ src/components/form-layout/form-layout.component.jsx
@@ -6,6 +6,7 @@ import style from './styles.module.scss';
 function FormLayout() {
   return (
     <main className={style.form__container}>
+      <h1 className="visually-hidden">multi step game subscription form</h1>
       <nav className={style.form__nav}>
         <div className={style.nav__container}>
           <FormNav />
